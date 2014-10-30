3729622990903bc0f553791cc1d234e6ecc1d883
val <estreguil.valentin@gmail.com>
2014-10-30 16:29:21 +0100

    GROSSE MAJ

---
+++ fct/param.c
@@ -0,0 +1,74 @@
+#include "../lib/includes.h"
+#include "../lib/param.h"
+
+
+/******************/
+/* Affiche l'aide */
+/******************/
+void notice(){
+	printf("Utilisation : creation_sequence -d entier -n entier -t entier -m motif\n\
+Description :\n\
+Exemple : creation_sequence -d 2 -n 50 -t 100 -m TATATGCA\n\
+OPTIONS :\n\
+	-d --nbErreur	entier : nombre de substitutions maximum autorisees\n\
+	-n --nbSeq	entier : nombre de sequences a creer\n\
+	-t --tailleSeq	entier : taille des sequences a creer\n\
+	-m --motif	chaine de caractere : motif a inserer dans la sequence\n\
+	\n\
+	-h --help	affiche cette aide\n\
+	\n");
+}
+
+
+
+/*********************/
+/* liste des options */
+/*********************/
+
+struct option long_options[] = {
+		{"nbErreur", required_argument, NULL, 'd'},
+		{"nbSeq", required_argument, NULL, 'n'},
+		{"tailleSeq", required_argument, NULL, 't'},
+		{"motif", required_argument, NULL, 'm'},
+		{"help", no_argument, 0,  'h' }
+};
+
+///////////////////////////////////////////////////
+/*Fonction permettant de récupérer les paramètres*/
+///////////////////////////////////////////////////
+
+void getParam (int* nbErreur, int* nbSeq, int* tailleSeq, char* motif, int argc, char *argv[]){
+	int opt = 0;
+	int long_index = 0; /* index des options */
+	if (argc<2)
+	{
+		fprintf(stderr, "ERREUR\n"); 
+		notice(); 
+		exit(1);
+	}
+	regex_t preg;
+	regcomp(&preg,"[^ATCG]+",REG_EXTENDED|REG_NOSUB);
+	while ((opt = getopt_long(argc, argv, "d:n:t:m:h", long_options, &long_index )) != -1) {
+		switch (opt) {
+			case 'd' : *nbErreur = atoi(optarg);
+				break;
+			case 'n' : *nbSeq = atoi(optarg);
+				break;
+			case 't' : *tailleSeq = atoi(optarg);
+				break;
+			case 'm' : strcpy(motif, optarg);
+				break;
+			case 'h' : notice(); exit(0);
+				break;
+			default  : fprintf(stderr, "Mauvais argument\n"); notice(); exit(1);
+				break;
+		}
+	}
+	if (!regexec(&preg,motif,0,0,0))
+	{
+		printf("[ERREUR] Le motif ne peux contenir que les caracteres A,T,C ou G\n");
+		notice();
+		exit(1);
+	}
+}
+
---
+++ lib/includes.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include <regex.h>
+#include <unistd.h>
+#include <getopt.h>
