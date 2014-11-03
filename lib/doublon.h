f2da85239deae0e48259c5dfa74d93d04037328f
val <estreguil.valentin@gmail.com>
2014-11-03 13:17:50 +0100

    séparation

--- lib/doublon.h
+++
@@ -1,6 +0,0 @@
-#ifndef MASQUE_H
-#define MASQUE_H
-#include "includes.h"
-bool estDoublon(int n, int *doublon, int count);
-
-#endif
--- lib/includes.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdbool.h>
-#include <string.h>
-#include <regex.h>
-#include <unistd.h>
-#include <getopt.h>
